2ba5dd2566f2040c57cb50e11e5033aa17e90c83
Greg <greg.distefano+github@gmail.com>
2016-03-06 01:40:00 +0100

    Added a basic test

--- tests.py
+++ tests.py
@@ -1,3 +1,17 @@
 import unittest
+import commands
 
-print "Hello"
+
+actual = commands.getoutput("./duplicate-files -min-size=1b -dir 'tests'")
+
+expected = commands.getoutput("cd tests && find -not -empty -type f -printf '%s\n' | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate")
+
+for i in expected.splitlines():
+    assert i.split()[0] in actual
+
+    filename = "".join(i.partition(' ')[1:]).strip()
+
+    if filename.startswith("."):
+        filename = filename.replace(".", "tests", 1)
+
+    assert filename in actual
